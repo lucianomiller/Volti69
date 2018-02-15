0090ada3895ea186d3bd5e7a2a10deb75176623a
MisaelRR8 <flashero.rerocho@gmail.com>
2018-02-15 11:14:19 -0800

    changed-comments

--- code/muxColor.vhd
+++ code/muxColor.vhd
@@ -1,12 +1,12 @@
 --	Multiplexor derminador de color (muxColor):						--
 --	Módulo para la determinación del color de todos los caracteres 	--
 --	a imprimir en pantalla basado en el siguiente código:			--
---	| rango de voltajes [V] | 	Color	   | RGB |					--
---	| 		0.00-0.99 		|	azul	   | 001 |					--
---	| 		1.00-1.99		|	verde	   | 010 |					--
---	| 		2.00-2.99		|	amarillo   | 110 |					--
---	| 		3.00-3.99		|	rojo 	   | 100 |					--
---	| 		4.00-inf	 	|   blanco 	   | 111 |					--
+--	| rango de voltajes [V] | 	Color	  	 | RGB |				--
+--	| 		0.00-0.99 		|	azul	 	 | 001 |				--
+--	| 		1.00-1.99		|	verde	   	 | 010 |				--
+--	| 		2.00-2.99		|	amarillo   	 | 110 |				--
+--	| 		3.00-3.99		|	rojo 	   	 | 100 |				--
+--	| 		4.00-inf	 	|   blanco 	     | 111 |				--
 --	Artista: Calcagno, Misael Dominique. Legajo: CyT-6322 			--
 
 library IEEE;
